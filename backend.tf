2f73d5a2094ab561bdeee9b13b8d573a36fec43a
John Walker <johnmichaelwalker2@gmail.com>
2025-08-30 15:36:13 +0100

    swtich backend to existing s3 bucket

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "k3s-lab-terraform-state"
+    bucket         = "boss-eks-lab-terraform-state"
     key            = "k3s-lab/terraform.tfstate"
     region         = "eu-west-2"
     dynamodb_table = "boss-eks-lab-terraform-locks"
