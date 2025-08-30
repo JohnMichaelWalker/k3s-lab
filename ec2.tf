8548c56e105a6336608dcb089c88afc2d24fc21a
John Walker <johnmichaelwalker2@gmail.com>
2025-08-30 15:38:56 +0100

    hard code the subnet id to default

--- ec2.tf
+++ ec2.tf
@@ -1,17 +1,7 @@
-# gets the default vpc
-data "aws_vpc" "default" {
-  default = true
-}
-
-# gets default subnet ids
-data "aws_subnet_ids" "default" {
-  vpc_id = data.aws_vpc.default.id
-}
-
 module "ec2_instance" {
   source        = "terraform-aws-modules/ec2-instance/aws"
   name          = "k3s-instance-1"
   instance_type = "t3.micro"
   key_name      = "user1"
-  subnet_id     = tolist(data.aws_subnet_ids.default.ids)[0]
+  subnet_id     = "subnet-0540e75f"
 }
