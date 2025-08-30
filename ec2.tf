8aa651cb344604cb3bfd91e65faa4566a711bd64
John Walker <johnmichaelwalker2@gmail.com>
2025-08-30 20:13:13 +0100

    switch key to k3s-lab

--- ec2.tf
+++ ec2.tf
@@ -2,6 +2,6 @@ module "ec2_instance" {
   source        = "terraform-aws-modules/ec2-instance/aws"
   name          = "k3s-instance-1"
   instance_type = "t3.micro"
-  key_name      = "user1"
+  key_name      = "k3s-lab"
   subnet_id     = "subnet-0540e75f"
 }
