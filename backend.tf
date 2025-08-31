e286fd0f09501e16d0cf1161136de053004cd708
John Walker <johnmichaelwalker2@gmail.com>
2025-08-31 11:30:49 +0100

    Big refactor to allow for multi terraform applies

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "boss-eks-lab-terraform-state"
-    key            = "k3s-lab/terraform.tfstate"
-    region         = "eu-west-2"
-    dynamodb_table = "boss-eks-lab-terraform-locks"
-    encrypt        = true
-  }
-}
---
+++ live/dev/control-plane-node/main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "eu-west-1"
+
+  default_tags {
+    tags = {
+      Project = "k3s-lab"
+      Environment = "dev"
+      ManagedBy = "terraform"
+      Role="control-plane-node"
+    }
+  }
+}
+
+module "control-plane-node" {
+  source = "../../../modules/control-plane-node"
+  vpc_cidr = "172.31.0.0/16"
+}
